61470688d0c2a03cb4b43130ff1d69b1c7dd6806
Iraklis Kanarkotis <irakliskanbusiness@gmail.com>
2025-07-23 22:26:15 +0300

    Add Streamlit web interface for todo app

---
+++ todolist/web.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import file_ops
+
+
+def add_todo():
+    todo_item = st.session_state["new_todo"]
+    if todo_item.strip():  # Only add if not empty
+        todos = file_ops.read_todos()
+        todos.append(todo_item + "\n")
+        file_ops.write_todos(todos)
+      
+
+todos = file_ops.read_todos()
+
+st.title("My Todo App")
+st.subheader("this is my todo app")
+st.write("This app allows you to manage your todo items interactively.")
+
+
+for todo in todos:
+    st.checkbox(todo)
+
+
+
+st.text_input(label="", placeholder="Add a new todo item",on_change=add_todo, key='new_todo')
